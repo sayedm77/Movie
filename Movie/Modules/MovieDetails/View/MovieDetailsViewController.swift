340ac86740077677629008af93037a1eeaad8b77
Elsayed Mansour <sayedmansoo123@gmail.com>
2024-09-29 14:26:46 +0300

    apply details screen by story board and view model

--- Movie/Modules/MovieDetails/View/MovieDetailsViewController.swift
+++ Movie/Modules/MovieDetails/View/MovieDetailsViewController.swift
@@ -9,12 +9,45 @@ import UIKit
 
 class MovieDetailsViewController: UIViewController {
 
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var movieReleaseDateLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var runtimeLabel: UILabel!
+    @IBOutlet weak var revenueLabel: UILabel!
+    @IBOutlet weak var genresNameLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    private var imageViewModel = MovieDetailsViewModel(with: nil)
+    var movieDetailsViewModel: MovieDetailsViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+              view.backgroundColor = .systemGray6
+              navigationController?.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: nil)
+              navigationController?.navigationBar.tintColor = UIColor.black
+              
+              configureUIElements()
     }
     
+     func configureUIElements(){
+         movieDetailsViewModel.fetchMovieDetails { movieDetails in
+             if let posterPath = movieDetails.posterPath {
+                 self.imageViewModel.downloadImage(posterPath: posterPath) { image in
+                     DispatchQueue.main.async {
+                         self.movieImageView.image = image
+                         self.movieNameLabel.text = movieDetails.title
+                         self.movieReleaseDateLabel.text = movieDetails.releaseDate
+                         self.rateLabel.text = String(format: "%.1f", movieDetails.voteAverage)
+                         self.runtimeLabel.text = "\(movieDetails.runtime) min"
+                         self.revenueLabel.text = "Revenue: \(movieDetails.revenue)$"
+                         self.overviewLabel.text = movieDetails.overview
+                         self.genresNameLabel.text = self.movieDetailsViewModel.setGenres(from: movieDetails.genres)
+                     }
+                 }
+             }
+         }
+     }
 
 
 }
--- Movie/Modules/MovieDetails/ViewModel/MovieDetailsViewModel.swift
+++ Movie/Modules/MovieDetails/ViewModel/MovieDetailsViewModel.swift
@@ -5,4 +5,54 @@
 //  Created by sayed mansour on 28/09/2024.
 //
 
-import Foundation
+
+import UIKit
+
+
+class MovieDetailsViewModel{
+    
+    private let movieID: Int!
+    private var movie: MovieDetails!
+    
+    init(with movieID: Int?) {
+        self.movieID = movieID
+    }
+    
+    func fetchMovieDetails(completion: @escaping (MovieDetails) -> Void){
+        MovieService.shared.getMovieDetails(for: movieID) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result{
+                
+            case .success(let movie):
+                self.movie = MovieDetails(id: movie.id, title: movie.title, releaseDate: movie.releaseDate, genres: movie.genres, overview: movie.overview, posterPath: movie.posterPath, runtime: movie.runtime, voteAverage: movie.voteAverage, revenue: movie.revenue)
+                completion(self.movie)
+            case .failure(let error):
+                print("error fetching moviesDetails \(error)")
+            }
+        }
+    }
+    
+    func downloadImage(posterPath: String, completion: @escaping (UIImage?) -> Void ){
+        MovieService.shared.downloadImage(from: posterPath) { [weak self] result in
+            guard let _ = self else { return  }
+            
+            switch result{
+            case .success(let image):
+                completion(image)
+                
+            case .failure(let error):
+                print("error downloading image \(error.localizedDescription)")
+                completion(nil)
+            }
+        }
+    }
+    
+    func setGenres(from genres: [Genre]) -> String {
+        let genreNames = genres.map { $0.name }
+        let concatenatedNames = genreNames.joined(separator: ", ")
+        return concatenatedNames
+    }
+    
+    
+}
